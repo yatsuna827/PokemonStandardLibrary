966ac9001249f096068e799e4f4974b66e29910a
yatsuna827 <45278736+yatsuna827@users.noreply.github.com>
2021-11-04 00:10:39 +0900

    テストコードを追加 (#4)
    * 性別比周りのテストを追加
    
    * 性別無しのテストと7世代のテストを追加

---
+++ UnitTest/TestData.cs
@@ -0,0 +1,340 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTest
+{
+    public class GenderRatioData
+    {
+        public static class MaleOnly
+        {
+            public static string[] Gen1 = new string[]
+            {
+                "ニドラン♂", "ニドリーノ", "ニドキング",
+                "ケンタロス",
+                "サワムラー", "エビワラー",
+            };
+            public static string[] Gen2 = new string[]
+            {
+                "バルキー", "カポエラー",
+            };
+            public static string[] Gen3 = new string[]
+            {
+                "バルビート",
+                "ラティオス",
+            };
+            public static string[] Gen4 = new string[]
+            {
+                "ガーメイル",
+                "エルレイド",
+            };
+            public static string[] Gen5 = new string[]
+            {
+                "ナゲキ",
+                "ダゲキ",
+                "ワシボン", "ウォーグル",
+                "トルネロス", "ボルトロス", "ランドロス",
+            };
+            public static string[] Gen6 = new string[] { };
+            public static string[] Gen7 = new string[] { };
+            public static string[] Gen8 = new string[]
+            {
+                "ベロバー", "ギモー", "オーロンゲ"
+            };
+        }
+        public static class M7F1
+        {
+            public static string[] Gen1 = new string[]
+            {
+                "フシギダネ", "フシギソウ", "フシギバナ",
+                "ヒトカゲ", "リザード", "リザードン",
+                "ゼニガメ", "カメール", "カメックス",
+                "イーブイ", "シャワーズ", "サンダース", "ブースター",
+                "オムナイト", "オムスター",
+                "カブト", "カブトプス",
+                "プテラ",
+                "カビゴン"
+            };
+            public static string[] Gen2 = new string[]
+            {
+                "チコリータ", "ベイリーフ", "メガニウム",
+                "ヒノアラシ", "マグマラシ", "バクフーン",
+                "ワニノコ", "アリゲイツ", "オーダイル",
+                "トゲピー", "トゲチック",
+                "エーフィ", "ブラッキー"
+            };
+            public static string[] Gen3 = new string[]
+            {
+                "キモリ", "ジュプトル", "ジュカイン",
+                "アチャモ", "ワカシャモ", "バシャーモ",
+                "ミズゴロウ", "ヌマクロー", "ラグラージ",
+                "リリーラ", "ユレイドル",
+                "アノプス", "アーマルド",
+                "ジーランス"
+            };
+            public static string[] Gen4 = new string[]
+            {
+                "ナエトル", "ハヤシガメ", "ドダイトス",
+                "ヒコザル", "モウカザル", "ゴウカザル",
+                "ポッチャマ", "ポッタイシ", "エンペルト",
+                "ズガイドス", "ラムパルド",
+                "タテトプス", "トリデプス",
+                "ミツハニー",
+                "リオル", "ルカリオ",
+                "リーフィア", "グレイシア",
+                "ゴンベ",
+                "トゲキッス"
+            };
+            public static string[] Gen5 = new string[]
+            {
+                "ツタージャ", "ジャノビー", "ジャローダ",
+                "ポカブ", "チャオブー", "エンブオー",
+                "ミジュマル", "フタチマル", "ダイケンキ",
+                "ヤナップ", "ヤナッキー",
+                "バオップ", "バオッキー",
+                "ヒヤップ", "ヒヤッキー",
+                "プロトーガ", "アバゴーラ",
+                "アーケン", "アーケオス",
+                "ゾロア", "ゾロアーク",
+            };
+            public static string[] Gen6 = new string[] 
+            {
+                "ハリマロン", "ハリボーグ", "ブリガロン",
+                "フォッコ", "テールナー", "マフォクシー",
+                "ケロマツ", "ゲコガシラ", "ゲッコウガ",
+                "チゴラス", "ガチゴラス",
+                "アマルス", "アマルルガ",
+                "ニンフィア",
+            };
+            public static string[] Gen7 = new string[] 
+            {
+                "モクロー", "フクスロー", "ジュナイパー",
+                "ニャビー", "ニャヒート", "ガオガエン",
+                "アシマリ", "オシャマリ", "アシレーヌ",
+                "ヤトウモリ",
+            };
+            public static string[] Gen8 = new string[]
+            {
+                "サルノリ", "バチンキー", "ゴリランダー",
+                "ヒバニー", "ラビフット", "エースバーン",
+                "メッソン", "ジメレオン", "インテレオン",
+                "ダクマ", "ウーラオス",
+            };
+        }
+        public static class M3F1
+        {
+            public static string[] Gen1 = new string[]
+            {
+                "ガーディ", "ウインディ",
+                "ケーシィ", "ユンゲラー", "フーディン",
+                "ワンリキー", "ゴーリキー", "カイリキー",
+                "エレブー",
+                "ブーバー",
+            };
+            public static string[] Gen2 = new string[]
+            {
+                "エレキッド",
+                "ブビィ"
+            };
+            public static string[] Gen3 = new string[]
+            {
+                "マクノシタ", "ハリテヤマ"
+            };
+            public static string[] Gen4 = new string[]
+            {
+                "エレキブル",
+                "ブーバーン"
+            };
+            public static string[] Gen5 = new string[]
+            {
+                "ドッコラー", "ドテッコツ", "ローブシン",
+            };
+            public static string[] Gen6 = new string[] { };
+            public static string[] Gen7 = new string[] { };
+            public static string[] Gen8 = new string[] { };
+        }
+        public static class FemaleOnly
+        {
+            public static string[] Gen1 = new string[]
+            {
+                "ニドラン♀", "ニドリーナ", "ニドクイン",
+                "ラッキー",
+                "ガルーラ",
+                "ルージュラ",
+            };
+            public static string[] Gen2 = new string[]
+            {
+                "ムチュール",
+                "ハピナス",
+                "ミルタンク",
+            };
+            public static string[] Gen3 = new string[]
+            {
+                "イルミーゼ",
+                "ラティアス",
+            };
+            public static string[] Gen4 = new string[]
+            {
+                "ピンプク",
+                "ミノマダム",
+                "ビークイン",
+                "ユキメノコ",
+                "クレセリア",
+            };
+            public static string[] Gen5 = new string[]
+            {
+                "チュリネ", "ドレディア",
+                "バルチャイ", "バルジーナ",
+            };
+            public static string[] Gen6 = new string[]
+            {
+                "フラベベ", "フラエッテ", "フラージェス",
+            };
+            public static string[] Gen7 = new string[]
+            {
+                "エンニュート",
+                "アマカジ", "アママイコ", "アマージョ",
+            };
+            public static string[] Gen8 = new string[] 
+            {
+                "ミブリム", "テブリム", "ブリムオン",
+                "マホミル", "マホイップ",
+            };
+        }
+        public static class M1F3
+        {
+            public static string[] Gen1 = new string[]
+            {
+                "ピッピ", "ピクシー",
+                "ロコン", "キュウコン",
+                "プリン", "プクリン",
+            };
+            public static string[] Gen2 = new string[]
+            {
+                "ピィ",
+                "ププリン",
+                "ブルー", "グランブル",
+                "サニーゴ",
+            };
+            public static string[] Gen3 = new string[]
+            {
+                "ルリリ",
+                "エネコ", "エネコロロ",
+                "ラブカス",
+            };
+            public static string[] Gen4 = new string[]
+            {
+                "ニャルマー", "ブニャット",
+            };
+            public static string[] Gen5 = new string[]
+            {
+                "チラーミィ", "チラチーノ",
+                "ゴチム", "ゴチミル", "ゴチルゼル",
+            };
+            public static string[] Gen6 = new string[] { };
+            public static string[] Gen7 = new string[] 
+            {
+                "オドリドリ",
+                "キュワワー",
+            };
+            public static string[] Gen8 = new string[] 
+            {
+                "サニゴーン", 
+            };
+        }
+        public static class Genderless
+        {
+            public static string[] Gen1 = new string[]
+            {
+                "コイル", "レアコイル",
+                "ビリリダマ", "マルマイン",
+                "ヒトデマン", "スターミー",
+                "メタモン",
+                "ポリゴン",
+                "フリーザー", "サンダー", "ファイヤー",
+                "ミュウツー", "ミュウ",
+            };
+            public static string[] Gen2 = new string[]
+            {
+                "ポリゴン2",
+                "アンノーン",
+                "ライコウ",
+                "エンテイ",
+                "スイクン",
+                "ルギア",
+                "ホウオウ",
+                "セレビィ",
+            };
+            public static string[] Gen3 = new string[]
+            {
+                "ヌケニン",
+                "ルナトーン", "ソルロック",
+                "ヤジロン", "ネンドール",
+                "ダンバル", "メタング", "メタグロス",
+                "レジロック", "レジアイス", "レジスチル",
+                "カイオーガ", "グラードン", "レックウザ",
+                "ジラーチ",
+                "デオキシス",
+            };
+            public static string[] Gen4 = new string[]
+            {
+                "ドーミラー", "ドータクン",
+                "ジバコイル",
+                "ポリゴンZ",
+                "ロトム",
+                "ユクシー", "エムリット", "アグノム",
+                "ディアルガ", "パルキア", "ギラティナ",
+                "レジギガス",
+                "フィオネ", "マナフィ",
+                "ダークライ",
+                "シェイミ",
+                "アルセウス",
+            };
+            public static string[] Gen5 = new string[]
+            {
+                "ビクティニ",
+                "ギアル", "ギギアル", "ギギギアル",
+                "フリージオ",
+                "ゴビット", "ゴルーグ",
+                "コバルオン", "テラキオン", "ビリジオン", "ケルディオ",
+                "レシラム", "ゼクロム", "キュレム",
+                "メロエッタ",
+                "ゲノセクト",
+            };
+            public static string[] Gen6 = new string[] 
+            {
+                "メレシー",
+                "ゼルネアス", "イベルタル", "ジガルデ",
+                "ディアンシー",
+                "フーパ",
+                "ボルケニオン",
+            };
+            public static string[] Gen7 = new string[]
+            {
+                "タイプ:ヌル", "シルヴァディ",
+                "メテノ",
+                "ダダリン",
+                "カプ・コケコ", "カプ・テテフ", "カプ・ブルル", "カプ・レヒレ",
+                "コスモッグ", "コスモウム", "ソルガレオ", "ルナアーラ",
+                "ウツロイド", "マッシブーン", "フェローチェ",
+                "デンジュモク", "テッカグヤ", "カミツルギ",
+                "アクジキング", "ネクロズマ",
+                "マギアナ",
+                "マーシャドー",
+                "ベベノム", "アーゴヨン",
+                "ツンデツンデ", "ズガドーン",
+                "ゼラオラ",
+            };
+            public static string[] Gen8 = new string[]
+            {
+                "ヤバチャ", "ポットデス",
+                "タイレーツ",
+                "パッチラゴン", "パッチルドン", "ウオノラゴン", "ウオチルドン",
+                "ザシアン", "ザマゼンタ", "ムゲンダイナ",
+                "ザルード",
+                "レジエレキ", "レジドラゴ",
+                "ブリザポス", "レイスポス", "バドレックス",
+            };
+        }
+    }
+}
