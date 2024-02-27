71912533e8e346c8880291ad4d498108d21d03a5
yatsuna827 <yatsuna_827@yahoo.co.jp>
2024-02-28 00:12:48 +0900

    feat: Gen3のIndividualのコンストラクタをpublicに解放

--- PokemonStandardLibrary.Gen3/Pokemon/Pokemon.Individual.cs
+++ PokemonStandardLibrary.Gen3/Pokemon/Pokemon.Individual.cs
@@ -12,23 +12,23 @@ namespace PokemonStandardLibrary.Gen3
             public string Name { get; }
             public string Form { get; }
             public uint Lv { get; }
+
             public uint PID { get; }
             public Nature Nature { get; }
             public Gender Gender { get; }
             public string Ability { get; }
-            public IReadOnlyList<uint> IVs { get; }
-            public IReadOnlyList<uint> EVs { get; }
-            public IReadOnlyList<uint> Stats { get; }
-            public ShinyType Shiny { get; private set; }
+
+            public uint[] IVs { get; } = new uint[6];
+            public uint[] EVs { get; } = new uint[6];
+            public uint[] Stats { get; } = new uint[6];
+
             public uint HiddenPower { get; }
             public PokeType HiddenPowerType { get; }
 
             public ShinyType GetShinyType(uint TSV)
                 => CommonFunctions.GetShinyType((PID >> 16) ^ (PID & 0xFFFF), TSV, 8);
-            public Individual SetShinyType(ShinyType value) { Shiny = value; return this; }
-            public Individual SetShinyType(uint TSV) { Shiny = GetShinyType(TSV); return this; }
 
-            internal protected Individual(Species species, uint pid, uint[] ivs, uint lv, uint[] evs = null)
+            public Individual(Species species, uint pid, uint[] ivs, uint lv, uint[] evs = null)
             {
                 Species = species;
                 Name = species.Name;
@@ -45,8 +45,24 @@ namespace PokemonStandardLibrary.Gen3
                 HiddenPowerType = CalcHiddenPowerType(ivs);
             }
 
-            public static Individual Empty = GetPokemon("Dummy").GetIndividual(0, new uint[6], 0);
-        }
+            public Individual(string name, uint pid, uint[] ivs, uint lv, uint[] evs = null)
+            {
+                var species = GetPokemon(name);
 
+                Species = species;
+                Name = species.Name;
+                Lv = lv;
+                Form = species.Form;
+                PID = pid;
+                IVs = ivs;
+                EVs = evs ?? new uint[6];
+                Nature = (Nature)(pid % 25);
+                Stats = GetStats(species.BS, ivs, evs, Nature, lv);
+                Ability = species.Ability[(int)(pid & 1)];
+                Gender = GetGender(pid & 0xFF, species.GenderRatio);
+                HiddenPower = CalcHiddenPower(ivs);
+                HiddenPowerType = CalcHiddenPowerType(ivs);
+            }
+        }
     }
 }
